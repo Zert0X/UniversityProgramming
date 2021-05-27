d5acabdc6aaeeb68933ff4f52459d35ec44e6253
Mark <kiramys133@gmail.com>
2021-05-28 01:45:36 +0300

    LR17 Z2

--- LR17/Z2/Z2.cpp
+++ LR17/Z2/Z2.cpp
@@ -2,19 +2,49 @@
 //
 
 #include <iostream>
-
+#include <string>
+#include <vector>
+#define MAX_COUNT 8
+using namespace std;
+struct PRICE {
+	string product_name;
+	string shop_name;
+	double price;
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+	system("ch1251");
+	system("cls");
+	vector <PRICE> products_list;
+	for (int i = 0; i < MAX_COUNT; i++) {
+		PRICE temp_product = {"","",0};
+		cout << "Product" << i + 1 << ":\n";
+		cout <<"Product name: ";					cin >> temp_product.product_name;
+		cout <<"Shop name(case sensitive): ";		cin >> temp_product.shop_name;
+		cout <<"Price: ";							cin >> temp_product.price;
+		cout << "-----------------\n";
+		products_list.push_back(temp_product);
+	}
+	for (int i = 0; i < MAX_COUNT; i++) {
+		for (int j = 0; j < MAX_COUNT - 1; j++) {
+			if (products_list[j].shop_name > products_list[j + 1].shop_name) {
+				PRICE tmp;
+				tmp = products_list[j];
+				products_list[j] = products_list[j + 1];
+				products_list[j + 1] = tmp;
+			}
+		}
+	}
+	system("cls");
+	string shop_name_search;
+	cout << "Shop name for product list(search is case sensitive):"; cin >> shop_name_search;
+	for (int i = 0; i < MAX_COUNT; i++) {
+		if (products_list[i].shop_name == shop_name_search){
+			cout << "Product" << i + 1	<< ":"; 
+			cout << "\nProduct name: "	<< products_list[i].product_name;
+			cout << "\nShop name: "		<< products_list[i].shop_name;
+			cout << "\nPrice: "			<< products_list[i].price<<" RUB";
+			cout << "\n-----------------";
+		}
+	}
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
